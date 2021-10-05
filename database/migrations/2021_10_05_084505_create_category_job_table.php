226a5002056d02dfa15746277a34a00a969b3285
Itsaraphap <itsaraphap.s@ku.th>
2021-10-05 10:08:55 +0700

    update categories and relationship many to many with job

---
+++ database/migrations/2021_10_05_084505_create_category_job_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryJobTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_job', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Category::class); //category_id
+            $table->foreignIdFor(\App\Models\Job::class); //job_id
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_job');
+    }
+}
