98ca52f02bd1f2e31f178723a38577c027aefe2b
pp_chjz <pirapat.j@ku.th>
2021-10-18 19:01:23 +0700

    job_backend

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 Route::apiResource('jobs',\App\Http\Controllers\Api\JobController::class);
-=======
-
 Route::apiResource('reviews', \App\Http\Controllers\Api\ReviewController::class)
     ->middleware('auth:api');
 Route::group([
@@ -32,5 +29,5 @@ Route::group([
     Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
     Route::post('refresh', [\App\Http\Controllers\Api\AuthController::class, 'refresh']);
     Route::post('me', [\App\Http\Controllers\Api\AuthController::class, 'me']);
+
 });
->>>>>>> d8e306351e6a096bbd2ee986dc79d31c7cf5e103
